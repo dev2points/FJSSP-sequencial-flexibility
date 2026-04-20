53340335f0cfbd0967877d45f6d471a4611f7780
dev2points <24020277@vnu.edu.vn>
2026-04-20 14:07:23 +0700

    run bottom up

--- bottom_up.sh
+++ bottom_up.sh
@@ -9,26 +9,26 @@ DARESULT_DIR=results/bottom_up/dafjs
 mkdir -p $YRESULT_DIR
 mkdir -p $DARESULT_DIR
 
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS01 2>&1 | tee $YRESULT_DIR/YFJS01.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS02 2>&1 | tee $YRESULT_DIR/YFJS02.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS03 2>&1 | tee $YRESULT_DIR/YFJS03.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS04 2>&1 | tee $YRESULT_DIR/YFJS04.log 
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS05 2>&1 | tee $YRESULT_DIR/YFJS05.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS06 2>&1 | tee $YRESULT_DIR/YFJS06.log 
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS07 2>&1 | tee $YRESULT_DIR/YFJS07.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS08 2>&1 | tee $YRESULT_DIR/YFJS08.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS09 2>&1 | tee $YRESULT_DIR/YFJS09.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS10 2>&1 | tee $YRESULT_DIR/YFJS10.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS11 2>&1 | tee $YRESULT_DIR/YFJS11.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS12 2>&1 | tee $YRESULT_DIR/YFJS12.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS13 2>&1 | tee $YRESULT_DIR/YFJS13.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS14 2>&1 | tee $YRESULT_DIR/YFJS14.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS15 2>&1 | tee $YRESULT_DIR/YFJS15.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS16 2>&1 | tee $YRESULT_DIR/YFJS16.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS17 2>&1 | tee $YRESULT_DIR/YFJS17.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS18 2>&1 | tee $YRESULT_DIR/YFJS18.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS19 2>&1 | tee $YRESULT_DIR/YFJS19.log
-./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS20 2>&1 | tee $YRESULT_DIR/YFJS20.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS01 2>&1 | tee $YRESULT_DIR/YFJS01.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS02 2>&1 | tee $YRESULT_DIR/YFJS02.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS03 2>&1 | tee $YRESULT_DIR/YFJS03.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS04 2>&1 | tee $YRESULT_DIR/YFJS04.log 
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS05 2>&1 | tee $YRESULT_DIR/YFJS05.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS06 2>&1 | tee $YRESULT_DIR/YFJS06.log 
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS07 2>&1 | tee $YRESULT_DIR/YFJS07.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS08 2>&1 | tee $YRESULT_DIR/YFJS08.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS09 2>&1 | tee $YRESULT_DIR/YFJS09.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS10 2>&1 | tee $YRESULT_DIR/YFJS10.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS11 2>&1 | tee $YRESULT_DIR/YFJS11.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS12 2>&1 | tee $YRESULT_DIR/YFJS12.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS13 2>&1 | tee $YRESULT_DIR/YFJS13.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS14 2>&1 | tee $YRESULT_DIR/YFJS14.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS15 2>&1 | tee $YRESULT_DIR/YFJS15.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS16 2>&1 | tee $YRESULT_DIR/YFJS16.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS17 2>&1 | tee $YRESULT_DIR/YFJS17.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS18 2>&1 | tee $YRESULT_DIR/YFJS18.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS19 2>&1 | tee $YRESULT_DIR/YFJS19.log
+# ./runlim -r $TO -s $MO python3 -u bottom_up.py $YDATA_DIR/YFJS20 2>&1 | tee $YRESULT_DIR/YFJS20.log
 
 ./runlim -r $TO -s $MO python3 -u bottom_up.py $DADATA_DIR/DAFJS01 2>&1 | tee $DARESULT_DIR/DAFJS01.log
 ./runlim -r $TO -s $MO python3 -u bottom_up.py $DADATA_DIR/DAFJS02 2>&1 | tee $DARESULT_DIR/DAFJS02.log
